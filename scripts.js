555a68da2b7ac5780a57ddafae70c3468d64f978
Welberrr <welberhenrique.costa@gmail.com>
2024-06-13 11:32:53 -0300

    Atualização de templates

--- scripts.js
+++ scripts.js
@@ -1,124 +1,168 @@
-$('#foto').on('input', function() {
-    var foto = $(this).val();
-    $("#preview-image-url").attr('src', foto);
-    $("#preview-image-url-alt").attr('src', foto);
-    $("#preview-image-url-third").attr('src', foto);
-});
+$(document).ready(function() {
+    $('#primeiroNome').on('input', function() {
+        var primeiroNome = $(this).val();
+        var segundoNome = $('#segundoNome').val();
+        $('#primeiroNomeAqui b').text(primeiroNome + ' ' + segundoNome);
+        $('#primeiroNomeAquiAlt').text(primeiroNome + ' ' + segundoNome);
+        $('#primeiroNomeAquiThird').text(primeiroNome + ' ' + segundoNome);
+    });
 
-$('#site').on('input', function() {
-    var primeiroNome = $('#primeiroNome').val();
-    var segundoNome = $('#segundoNome').val();
-    var nomeCompleto = primeiroNome + '-' + segundoNome;
-    var site = $(this).val();
-    $('#siteAqui').text(site);
-    $('#siteAqui').attr('href', site + '?utm_source=assinatura&utm_medium=' + nomeCompleto);
-    $('#siteAquiAlt').text(site);
-    $('#siteAquiAlt').attr('href', site + '?utm_source=assinatura&utm_medium=' + nomeCompleto);
-    $('#siteAquiThird').text(site);
-    $('#siteAquiThird').attr('href', site + '?utm_source=assinatura&utm_medium=' + nomeCompleto);
-});
+    $('#segundoNome').on('input', function() {
+        var primeiroNome = $('#primeiroNome').val();
+        var segundoNome = $(this).val();
+        $('#primeiroNomeAqui b').text(primeiroNome + ' ' + segundoNome);
+        $('#primeiroNomeAquiAlt').text(primeiroNome + ' ' + segundoNome);
+        $('#primeiroNomeAquiThird').text(primeiroNome + ' ' + segundoNome);
+    });
 
-$('#linkedin').on('input', function() {
-    var linkedin = $(this).val();
-    $('#linkedinAqui').attr('href', linkedin);
-    $('#linkedinAquiAlt').attr('href', linkedin);
-});
+    $('#cargo').on('input', function() {
+        var cargo = $(this).val();
+        $('#cargoAqui').text(cargo);
+        $('#cargoAquiAlt').text(cargo);
+        $('#cargoAquiThird').text(cargo);
+    });
 
-$('#linkedinIco').on('input', function() {
-    var linkedinIco = $(this).val();
-    $(".linkedinIco").attr('src', linkedinIco);
-    $("#preview-linkedin-url-alt").attr('src', linkedinIco);
-});
+    $('#telefone').on('input', function() {
+        var telefone = $(this).val();
+        $('a#telefoneAqui').text('+55 ' + telefone);
+        $('#telefoneAqui').attr('href', 'tel:' + telefone.replace(/[^\d]+/g, ''));
+        $('a#telefoneAquiAlt').text('+55 ' + telefone);
+        $('#telefoneAquiAlt').attr('href', 'tel:' + telefone.replace(/[^\d]+/g, ''));
+        $('#telefoneAquiThird').text('+55 ' + telefone);
+    });
 
-$('#instagram').on('input', function() {
-    var instagram = $(this).val();
-    $('#instagramAqui').attr('href', instagram);
-    $('#instagramAquiAlt').attr('href', instagram);
-});
+    $('#email').on('input', function() {
+        var email = $(this).val();
+        $('a#emailAqui').text(email);
+        $('#emailAqui').attr('href', 'mailto:' + email);
+        $('a#emailAquiAlt').text(email);
+        $('#emailAquiAlt').attr('href', 'mailto:' + email);
+        $('a#emailAquiThird').text(email);
+        $('#emailAquiThird').attr('href', 'mailto:' + email);
+    });
 
-$('#instagramIco').on('input', function() {
-    var instagramIco = $(this).val();
-    $(".instagramIco").attr('src', instagramIco);
-    $("#preview-instagram-url-alt").attr('src', instagramIco);
-});
+    $('#foto').on('input', function() {
+        var foto = $(this).val();
+        $("#preview-image-url").attr('src', foto);
+        $("#preview-image-url-alt").attr('src', foto);
+        $("#preview-image-url-third").attr('src', foto);
+    });
 
-$('#facebook').on('input', function() {
-    var facebook = $(this).val();
-    $('#facebookAqui').attr('href', facebook);
-    $('#facebookAquiAlt').attr('href', facebook);
-});
+    $('#site').on('input', function() {
+        var primeiroNome = $('#primeiroNome').val();
+        var segundoNome = $('#segundoNome').val();
+        var nomeCompleto = primeiroNome + '-' + segundoNome;
+        var site = $(this).val();
+        $('#siteAqui').text(site);
+        $('#siteAqui').attr('href', site + '?utm_source=assinatura&utm_medium=' + nomeCompleto);
+        $('#siteAquiAlt').text(site);
+        $('#siteAquiAlt').attr('href', site + '?utm_source=assinatura&utm_medium=' + nomeCompleto);
+        $('#siteAquiThird').text(site);
+        $('#siteAquiThird').attr('href', site + '?utm_source=assinatura&utm_medium=' + nomeCompleto);
+    });
 
-$('#facebookIco').on('input', function() {
-    var facebookIco = $(this).val();
-    $(".facebookIco").attr('src', facebookIco);
-    $("#preview-facebook-url-alt").attr('src', facebookIco);
-});
+    $('#linkedin').on('input', function() {
+        var linkedin = $(this).val();
+        $('#linkedinAqui').attr('href', linkedin);
+        $('#linkedinAquiAlt').attr('href', linkedin);
+    });
 
-$("#clickToChange").on('click', function() {
-    var hexadecimal = $('#hexadecimal').val();
-    $('table').css('border-color', hexadecimal);
-    $('#primeiroNomeAqui').css('color', hexadecimal);
-    $('#segundoNome').css('color', hexadecimal);
-    $('.signature-name').css('color', hexadecimal);
-    $('.right-section .name').css('color', hexadecimal);
-    $('.right-section .contact').css('color', hexadecimal);
-    $('.signature').css('border-color', hexadecimal);
-    $('.left-section').css('border-right-color', hexadecimal);
+    $('#linkedinIco').on('input', function() {
+        var linkedinIco = $(this).val();
+        $(".linkedinIco").attr('src', linkedinIco);
+        $("#preview-linkedin-url-alt").attr('src', linkedinIco);
+    });
+
+    $('#instagram').on('input', function() {
+        var instagram = $(this).val();
+        $('#instagramAqui').attr('href', instagram);
+        $('#instagramAquiAlt').attr('href', instagram);
+    });
+
+    $('#instagramIco').on('input', function() {
+        var instagramIco = $(this).val();
+        $(".instagramIco").attr('src', instagramIco);
+        $("#preview-instagram-url-alt").attr('src', instagramIco);
+    });
+
+    $('#facebook').on('input', function() {
+        var facebook = $(this).val();
+        $('#facebookAqui').attr('href', facebook);
+        $('#facebookAquiAlt').attr('href', facebook);
+    });
+
+    $('#facebookIco').on('input', function() {
+        var facebookIco = $(this).val();
+        $(".facebookIco").attr('src', facebookIco);
+        $("#preview-facebook-url-alt").attr('src', facebookIco);
+    });
+
+    $("#clickToChange").on('click', function() {
+        var hexadecimal = $('#hexadecimal').val();
+        $('table').css('border-color', hexadecimal);
+        $('#primeiroNomeAqui').css('color', hexadecimal);
+        $('#segundoNome').css('color', hexadecimal);
+        $('.signature-name').css('color', hexadecimal);
+        $('.right-section .name').css('color', hexadecimal);
+        $('.right-section .contact').css('color', hexadecimal);
+        $('.signature').css('border-color', hexadecimal);
+        $('.left-section').css('border-right-color', hexadecimal);
+    });
 });
 
 function copy() {
-var target = document.getElementById('ass');
-var range, select;
-if (document.createRange) {
-    range = document.createRange();
-    range.selectNode(target);
-    select = window.getSelection();
-    select.removeAllRanges();
-    select.addRange(range);
-    document.execCommand('copy');
-    select.removeAllRanges();
-} else {
-    range = document.body.createTextRange();
-    range.moveToElementText(target);
-    range.select();
-    document.execCommand('copy');
-}
+    var target = document.getElementById('ass');
+    var range, select;
+    if (document.createRange) {
+        range = document.createRange();
+        range.selectNode(target);
+        select = window.getSelection();
+        select.removeAllRanges();
+        select.addRange(range);
+        document.execCommand('copy');
+        select.removeAllRanges();
+    } else {
+        range = document.body.createTextRange();
+        range.moveToElementText(target);
+        range.select();
+        document.execCommand('copy');
+    }
 }
 
 function copyAlt() {
-var target = document.getElementById('assAlt');
-var range, select;
-if (document.createRange) {
-    range = document.createRange();
-    range.selectNode(target);
-    select = window.getSelection();
-    select.removeAllRanges();
-    select.addRange(range);
-    document.execCommand('copy');
-    select.removeAllRanges();
-} else {
-    range = document.body.createTextRange();
-    range.moveToElementText(target);
-    range.select();
-    document.execCommand('copy');
-}
+    var target = document.getElementById('assAlt');
+    var range, select;
+    if (document.createRange) {
+        range = document.createRange();
+        range.selectNode(target);
+        select = window.getSelection();
+        select.removeAllRanges();
+        select.addRange(range);
+        document.execCommand('copy');
+        select.removeAllRanges();
+    } else {
+        range = document.body.createTextRange();
+        range.moveToElementText(target);
+        range.select();
+        document.execCommand('copy');
+    }
 }
 
 function copyThird() {
-var target = document.getElementById('assThird');
-var range, select;
-if (document.createRange) {
-    range = document.createRange();
-    range.selectNode(target);
-    select = window.getSelection();
-    select.removeAllRanges();
-    select.addRange(range);
-    document.execCommand('copy');
-    select.removeAllRanges();
-} else {
-    range = document.body.createTextRange();
-    range.moveToElementText(target);
-    range.select();
-    document.execCommand('copy');
-}
+    var target = document.getElementById('assThird');
+    var range, select;
+    if (document.createRange) {
+        range = document.createRange();
+        range.selectNode(target);
+        select = window.getSelection();
+        select.removeAllRanges();
+        select.addRange(range);
+        document.execCommand('copy');
+        select.removeAllRanges();
+    } else {
+        range = document.body.createTextRange();
+        range.moveToElementText(target);
+        range.select();
+        document.execCommand('copy');
+    }
 }
