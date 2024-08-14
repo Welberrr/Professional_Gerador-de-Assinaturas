e3453b3b45f6c943460d700ed7123632328a9133
Welber Henrique <welberhenrique.costa@gmail.com>
2024-08-14 17:10:54 -0300

    atualizado

---
+++ index.js
@@ -0,0 +1,72 @@
+$(document).ready(function() {
+    // Máscara de telefone para o formato (xx) x xxxx-xxxx
+    $('#telefone').mask('(00) 0 0000-0000');
+
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+    }
+
+    function formatNameAndSurname() {
+        var nome = $('#nome').val();
+        var sobrenome = $('#sobrenome').val();
+        
+        if (nome) {
+            $('#nome').val(capitalizeFirstLetter(nome));
+        }
+        if (sobrenome) {
+            $('#sobrenome').val(capitalizeFirstLetter(sobrenome));
+        }
+    }
+
+    function updateEmail() {
+        var nome = $('#nome').val().toLowerCase().replace(/\s/g, '');
+        var sobrenome = $('#sobrenome').val().toLowerCase().replace(/\s/g, '');
+        if (nome && sobrenome) {
+            var email = nome + '.' + sobrenome + '@referenciaseguros.com.br';
+            $('#email').val(email);
+        }
+    }
+
+    $('#nome, #sobrenome').on('input', function() {
+        formatNameAndSurname();
+        updateEmail();
+    });
+
+    // Função para mostrar a seção correspondente
+    function showSection(sectionId) {
+        // Esconde todas as seções
+        $('.section').hide();
+
+        // Mostra apenas a seção clicada
+        $('#' + sectionId).show();
+    }
+
+    // Evento para os botões do menu
+    $('.menu button').click(function() {
+        var sectionId = $(this).attr('data-section');
+        showSection(sectionId);
+    });
+
+    // Mostra a primeira seção ao carregar a página
+    showSection('insercao-dados');
+
+    // Função para atualizar os modelos com os dados inseridos
+    $('#nome, #sobrenome, #cargo, #telefone').on('input', function() {
+        var nome = $('#nome').val();
+        var sobrenome = $('#sobrenome').val();
+        var cargo = $('#cargo').val();
+        var telefone = $('#telefone').val();
+
+        // Atualiza os modelos
+        $('.modelo p:nth-child(1)').text(nome + ' ' + sobrenome);
+        $('.modelo p:nth-child(2)').text(cargo);
+        $('.modelo p:nth-child(3)').text(telefone);
+
+        // Atualiza o link do WhatsApp para abrir no WhatsApp Web
+        if (telefone) {
+            var formattedPhone = telefone.replace(/\D/g, ''); // Remove todos os caracteres não numéricos
+            var whatsappLink = "https://web.whatsapp.com/send?phone=55" + formattedPhone;
+            $('.whatsapp-link').attr('href', whatsappLink);
+        }
+    });
+});
